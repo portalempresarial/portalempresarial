fe6fb89f353f397b0211327e24629fc14e55d661
miripower <polmivi@gmail.com>
2025-04-04 15:49:30 +0200

    hotfix: market

--- resources/views/livewire/sections/authorized/market.blade.php
+++ resources/views/livewire/sections/authorized/market.blade.php
@@ -64,7 +64,7 @@ foreach ($companiesList as $company) {
 
     <div class="flex-1 flex flex-col gap-10">
         @foreach ($companiesList as $company)
-            <div class="p-5 border border-gray-300 rounded-md shadow-md bg-white mx-24">
+            <div class="p-5 border border-gray-300 rounded-md shadow-md bg-white lg:mx-24">
                 <div class="flex items-center gap-5">
                     @if ($company->icon)
                         <img class="max-w-[70px] rounded-sm h-[50px]"
